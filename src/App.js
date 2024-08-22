56336ac348518fbba0175ae806b2ff2dbbabd1e7
Marivanenoronha <noronhamarivane4@gmail.com>
2024-08-22 14:08:16 -0300

    feat: change app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Container  from 'react-bootstrap/container';
+import Container  from 'react-bootstrap/Container';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Home from "./pages/home";
