9d0e66af3565a74fbfdbb03ac69cbbca32054de8
Marivanenoronha <noronhamarivane4@gmail.com>
2024-08-22 14:29:58 -0300

    feat|: change menu-page h1

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -98,7 +98,7 @@ function Menu() {
         <div className="menu-page">
             <header className="mt-5">
                 <div className="container h-100 d-flex align-items-center justify-content-center">
-                    <h1 className="text-light">Menu</h1>
+                    <h1 className="text-light">Cardápio</h1>
                 </div>
             </header>
 
