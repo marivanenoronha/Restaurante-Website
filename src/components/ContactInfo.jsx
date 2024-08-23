0b9fe0c44c5a4c80fc204a808289196720365f14
Marivanenoronha <noronhamarivane4@gmail.com>
2024-08-23 10:44:16 -0300

    feat: change files

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -5,7 +5,7 @@ export function ContactInfo() {
         <div className="d-flex flex-column align-items-center">
                 <h2 className="fs-1 mb-3 text-uppercase fw-bold">Onde nos encontrar</h2>
                 <p className="mb-5 ">Rua Amaro José Pereira, Ararangua - sc</p>
-                <h3 className="">Horário de funcionamento</h3>
+                <h3 className="mb-2">Horário de funcionamento</h3>
                 <p className="m-0">Segunda - Domingo  Almoço11:00 - 15:00  Janta19:00 - 23:00 </p>
                
         </div>
