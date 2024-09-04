4f9d10080899d309473c2ae95dba4955182422c5
Marivanenoronha <noronhamarivane4@gmail.com>
2024-09-04 19:43:36 -0300

    feat: change containfo.jsx

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,7 +4,9 @@ export function ContactInfo() {
     return (
         <div className="d-flex flex-column align-items-center">
             <h2 className=" mb-3 text-uppercase fw-bold text-dark">Onde nos encontrar</h2>
-            <p className="mb-5 text-dark">Rua Amaro José Pereira, Ararangua-sc<br></br> E-mail: noronhamarivane4@gmail.com<br></br> Contato: 48 991047501
+            <p className="mb-5 text-dark"><i className="fas fa-map-marker-alt me-2"></i> Rua Amaro José Pereira, Ararngua SC<br></br>
+                <i className="fas fa-envelope me-2"></i> noronhamarivane4@gmail.com<br></br>
+                <i className="fab fa-whatsapp me-2"></i> 48 991740401
                 <div className="contact-icons mt-4 d-flex justify-content-around w-70">
                     <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
                         <i className="fab fa-facebook-f text-dark fa-2x"></i>
@@ -20,9 +22,23 @@ export function ContactInfo() {
                     </a>
                 </div>
             </p>
-            <h3 className="mb-2 text-dark">Horário de funcionamento</h3>
-            <p className="m-0 text-dark">Segunda - Domingo  <br></br>Almoço11:00 - 15:00
-                Janta19:00 - 23:00 </p>
+            <div className="operating-hours ">
+                <h3 className="mb-3 text-dark fw-bold">Horário de Funcionamento</h3>
+                <div className="hours">
+                    <p className="day text-dark mb-2">
+                        <i className="fas fa-calendar-day me-2 text-dark"></i>
+                        Quarta - Domingo
+                    </p>
+                    <p className="time text-dark">
+                        <i className="fas fa-utensils me-2 text-dark"></i>
+                        Almoço: <span className="fw-bold">11:00 - 15:00</span>
+                    </p>
+                    <p className="time text-dark">
+                        <i className="fas fa-moon me-2 text-dark"></i>
+                        Janta: <span className="fw-bold">19:00 - 23:00</span>
+                    </p>
+                </div>
+            </div>
         </div >
     )
 }
